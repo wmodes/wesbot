28e2adcec218122294ea6cd8d142bb03066d19b3
Wes Modes <wmodes@gmail.com>
2023-10-16 15:20:33 -0700

    testing

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-#from wesbot import app as application
+from wesbot import app as application
 
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    body = ['<!DOCTYPE html><html><meta charset="utf-8">',
-            '<title>It works!</title><h1>It works!</h1>']
-    return [line.encode('utf-8') for line in body]
+# def application(env, start_response):
+#     start_response('200 OK', [('Content-Type','text/html')])
+#     body = ['<!DOCTYPE html><html><meta charset="utf-8">',
+#             '<title>It works!</title><h1>It works!</h1>']
+#     return [line.encode('utf-8') for line in body]
