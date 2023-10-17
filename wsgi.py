d0e6505d926c6e35d2988dd232b276d64701d5bc
Wes Modes <wmodes@gmail.com>
2023-10-16 20:24:29 -0700

    debug

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
-# from wesbot import app
-
-# application = app()
-
 from wesbot import app
 
-def application():
-  return app
+application = app()
+
+# from wesbot import app
+
+# def application():
+#   return app
+
+# if __name__ == "__main__":
+#     application.run()
